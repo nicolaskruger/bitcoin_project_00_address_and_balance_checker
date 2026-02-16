268c07caa1a0760837899b85530fd4e1dbff1889
ni <nicolas.nkruger@gmail.com>
2026-02-16 09:15:49 -0300

    save

--- src/infra/http/blockchain_controller.rs
+++ src/infra/http/blockchain_controller.rs
@@ -1,10 +1,23 @@
 use actix_web::{HttpResponse, Responder, get, web::ServiceConfig};
 
+use crate::infra::repository::blockchain_repository::fetch_blockchain_info;
+
 #[get("/blockchain/tail")]
 pub async fn blockchain_tail() -> impl Responder {
     HttpResponse::Ok().body("blockchain_tail")
 }
 
+#[get("/blockchain/info")]
+pub async fn blockchain_info() -> impl Responder {
+    let fetch = fetch_blockchain_info().await;
+
+    match fetch {
+        Ok(res) => HttpResponse::Ok().json(res),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
 pub fn blockchain_controller(app: &mut ServiceConfig) {
     app.service(blockchain_tail);
+    app.service(blockchain_info);
 }
